27ae191a69915f738aa180c6f8efa7f6fcbecc38
javier guedez <leidylore05@gmail.com>
2023-03-27 18:56:14 -0500

    Hoja de vida

--- diccionarios.py
+++ diccionarios.py
@@ -1,12 +1,22 @@
-input("Ingrese los datos:   ")
-Nom=input("Ingrese el nombre: ")
-edad=int(input("Ingrese la edad: "))
-dir=input("Ingrese la direccion: ")
-tel=int(input("Ingrese el telefono: "))
-d={"Nombre":" ", "Edad":" ","Dir": " ","Tel": " "}
+repetir="S"
+count=0
+d={}
+while repetir=="S" or repetir=="s":
+    l=[]
+    Nom=input("Ingrese el nombre: ")
+    l.append(Nom)
+    edad=int(input("Ingrese la edad: "))
+    l.append(edad)
+    dir=input("Ingrese la direccion: ")
+    l.append(dir)
+    tel=int(input("Ingrese el telefono: "))
+    l.append(tel)
+    Sexo=input("Ingrese Sexo: ")
+    l.append(Sexo)
+    d[count]=l
+    count+=1
+    repetir=input("¿Desea continuar S/N?")
+    
 print(d)
-d["Nombre"]=Nom
-d["Edad"]=edad
-d["Dir"]=dir
-d["Tel"]=tel
-print(d)
+
+
