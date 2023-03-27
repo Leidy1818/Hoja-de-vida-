265dee99468165029e2e3e2c9ee6787e5671d6ae
javier guedez <leidylore05@gmail.com>
2023-03-26 19:44:19 -0500

    Hoja de vida

---
+++ diccionarios.py
@@ -0,0 +1,12 @@
+input("Ingrese los datos:   ")
+Nom=input("Ingrese el nombre: ")
+edad=int(input("Ingrese la edad: "))
+dir=input("Ingrese la direccion: ")
+tel=int(input("Ingrese el telefono: "))
+d={"Nombre":" ", "Edad":" ","Dir": " ","Tel": " "}
+print(d)
+d["Nombre"]=Nom
+d["Edad"]=edad
+d["Dir"]=dir
+d["Tel"]=tel
+print(d)
