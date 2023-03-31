83b90ff259201da072c618637ff5239e729739a3
javier guedez <leidylore05@gmail.com>
2023-03-30 19:52:21 -0500

    actividad4

---
+++ actividad4/puntouno.py
@@ -0,0 +1,7 @@
+C=float(input("Ingrece grados en Celsius: "))
+F=(C*9/5)+32
+K=C+273.15
+Ra=(C*9/5)+491.67
+print(C, "es equivalente a: ", F, "grados Fahrenheit")
+print(C, "es equivalente a: ", Ra, "grados Rankine")
+print(C, "es equivalente a: ", K, "grados kelvin")
